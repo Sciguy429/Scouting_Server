9540d62bd720194dda4dc3408a01d650ec80a6d2
Christopher KIng <sciguy429@gmail.com>
2018-02-02 16:09:12 -0600

    Finalized Sync Command Params

--- src/com/Sciguy429/ScoutingServer/BTCommand.java
+++ src/com/Sciguy429/ScoutingServer/BTCommand.java
@@ -3,8 +3,11 @@ package com.Sciguy429.ScoutingServer;
 public class BTCommand {
 
     //Data Storage Object For BT Command Info To Be Stored Into After It Has Been Parsed
+    //Commands Can Have Arguments Attached As Well As References To Allow for Responses
 
-    public static final class ValidateLogin extends BTCommand {  //Checks Login Information Against Database And Returns User Information
+    public static final class ValidateLogin extends BTCommand {
+        //Checks Login Information Against Database And Returns User Information
+        //Requires A Username And Password
         String userName;
         String userPassword;
         public ValidateLogin(String userName, String userPassword) {
@@ -13,9 +16,19 @@ public class BTCommand {
         }
     }
 
-    public static final class Sync extends BTCommand {  //Requests A Full Database Re-Sync From The Server
-        public Sync() {
-            //No Special Options Needed For A Simple Sync Request
+    public static final class Sync extends BTCommand {
+        //Requests A Database Re-Sync From The Server
+        //If Full Is Set To True The Re-Sync Sends All Data Regardless Of The Condition Of the Remote Devices Internal Database
+        //Otherwise A Md5 Of The Clients Config DB Copy Is Sent, Just To Make Sure A Data Sync Is Possible
+        //NOTE: A Full DB Sync Will Overwrite The Clients Config Table UNCONDITIONALLY And Thus Will Only Be Used On First App Start Or After A Hard DB Reset On The Client
+        Boolean full = false;
+        String clientConfigDBMd5;
+        public Sync(Boolean full) {
+            this.full = full;
+        }
+
+        public Sync(String clientConfigDBMd5) {
+            this.clientConfigDBMd5 = clientConfigDBMd5;
         }
     }
 }
