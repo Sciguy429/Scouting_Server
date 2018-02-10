5d96be30b4af6ddf61a987dd75127545bc2a6fef
Christopher KIng <sciguy429@gmail.com>
2018-02-10 07:34:19 -0600

    BT Connection System

--- src/com/Sciguy429/ScoutingServer/BTConnectionThread.java
+++ src/com/Sciguy429/ScoutingServer/BTConnectionThread.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class BTConnectionThread implements Runnable {
 
     private int connectionNumber;
+    private String connectionLogTag;
     private Connection H2Connection;
     private StreamConnection BTConnection;
     private InputStream BTIS;
@@ -16,24 +18,46 @@ public class BTConnectionThread implements Runnable {
 
     BTConnectionThread(int connectionNumber, Connection H2Connection, StreamConnection BTConnection) {
         this.connectionNumber = connectionNumber;
+        this.connectionLogTag = "Connection " + connectionNumber + ": ";
         this.H2Connection = H2Connection;
         this.BTConnection = BTConnection;
     }
 
     @Override
     public void run() {
-        System.out.println("Connection " + connectionNumber + ": Thread Start");
+        System.out.println(connectionLogTag + "Thread Start");
+        ArrayList<byte[]> command = new ArrayList<>();
         try {
             BTIS = BTConnection.openInputStream();
             BTOS = BTConnection.openOutputStream();
             while (true) {
                 if (BTIS.available() > 0) {
-                    System.out.println(BTIS.read());
+                    command.clear();
+                    while(BTIS.available() > 0) {
+                        command.add(toBytes(BTIS.read()));
+                    }
+                    System.out.print(connectionLogTag + "Command Received: ");
+                    for (byte[] b : command) {
+                        System.out.print(new String(b, "UTF-8"));
+                    }
+                    System.out.print("\r\n");
                 }
                 Thread.sleep(10);
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private byte[] toBytes(int i)
+    {
+        byte[] result = new byte[4];
+
+        result[0] = (byte) (i >> 24);
+        result[1] = (byte) (i >> 16);
+        result[2] = (byte) (i >> 8);
+        result[3] = (byte) (i /*>> 0*/);
+
+        return result;
     }
 }
--- src/com/Sciguy429/ScoutingServer/Main.java
+++ src/com/Sciguy429/ScoutingServer/Main.java
@@ -9,11 +9,21 @@ public class Main {
         try {
             Class.forName("org.h2.Driver");
             Connection conn = DriverManager.getConnection("jdbc:h2:~/scouting", "JAVAADMIN", "%Lc3W+z~Y`sQ*?Zk"); //Connect To The H2 DB With JAVAADMIN
+            isDatabaseValid(conn);
             new Thread(new BTManagerThread(conn)).start();
             while(true) {}
             //conn.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    private static boolean isDatabaseValid(Connection conn) {
+        try {
+            conn.getClientInfo();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false; //Sql issue so database is definitely broke
+        }
+        return true;
     }
 }
